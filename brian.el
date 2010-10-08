f1602578158b3c50f875ce02d7a3e0efe94e8c82
brianc <brian.m.carlson@gmail.com>
2010-10-08 13:20:19 -0500

    added customizations

---
+++ brian.el
@@ -0,0 +1,13 @@
+(setenv "PATH"
+        (concat
+         "/usr/local/bin:"
+         (getenv "PATH")))
+
+(push "/usr/local/bin" exec-path)
+
+(set-face-attribute 'default nil :height 160)
+(if window-system (color-theme-zenburn))
+(set-frame-width (selected-frame) 150)
+(set-frame-height (selected-frame) 50)
+(set-frame-position (selected-frame) 40 40)
+
