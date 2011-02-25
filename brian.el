c92f26f896f5b87a0d5c23270187ff8936756356
Brian Carlson <brian.m.carlson@gmail.com>
2011-02-25 12:13:12 -0600

    only adjust frame for window mode

--- brian.el
+++ brian.el
@@ -6,9 +6,9 @@
 
 (push "/usr/local/bin" exec-path)
 
-(set-face-attribute 'default nil :height 170)
-(if window-system (color-theme-zenburn))
-(set-frame-position (selected-frame) 40 40)
-(set-frame-width (selected-frame) 150)
-(set-frame-height (selected-frame) 40)
-(set-frame-parameter (selected-frame) 'cursor-type 'bar)
+(if window-system
+    (lambda ()((set-frame-position (selected-frame) 40 40)
+          (set-face-attribute 'default nil :height 170)
+          (set-frame-height (selected-frame) 40)
+          (set-frame-width (selected-frame) 150)
+          (set-frame-parameter (selected-frame) 'cursor-type 'bar))))
