5bb8865adcca51bcfec0041230be98f64260858a
brianc <brian.m.carlson@gmail.com>
2011-03-05 11:09:13 -0600

    always eval frame stuff

--- brian.el
+++ brian.el
@@ -6,9 +6,8 @@
 
 (push "/usr/local/bin" exec-path)
 
-(if window-system
-    (lambda ()((set-frame-position (selected-frame) 40 40)
-          (set-face-attribute 'default nil :height 170)
-          (set-frame-height (selected-frame) 40)
-          (set-frame-width (selected-frame) 150)
-          (set-frame-parameter (selected-frame) 'cursor-type 'bar))))
+(set-frame-position (selected-frame) 40 40)
+(set-face-attribute 'default nil :height 150)
+(set-frame-height (selected-frame) 40)
+(set-frame-width (selected-frame) 150)
+(set-frame-parameter (selected-frame) 'cursor-type 'bar)
