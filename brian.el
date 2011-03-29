a1a195428c198dd0302eaaa95baecf42f5db6c94
Brian Carlson <brian.m.carlson@gmail.com>
2011-03-29 09:42:53 -0500

    updated paths for node + eshell

--- brian.el
+++ brian.el
@@ -1,8 +1,14 @@
 ;;*nix specific stuff
 (setenv "PATH"
         (concat
+         "/home/brian/local/bin:"
+         "/home/brian/node_modules/.bin/:"
+         "/home/brian/local/node/bin:"
          "/usr/local/bin:"
          (getenv "PATH")))
+
+(setenv "PREFIX" "/home/brian/local")
+(setenv "N_PREFIX" "/home/brian/local/n")
 
 (push "/usr/local/bin" exec-path)
 
